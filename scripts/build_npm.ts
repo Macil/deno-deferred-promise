937f1cafda334c7d1b8ea8ad703afad25214c231
Chris Cowan <agentme49@gmail.com>
2022-04-24 03:15:49 -0700

    +npm instructions

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,3 +1,6 @@
+// npm publishing instructions:
+//  deno run -A scripts/build_npm.ts [VERSION]
+//  cd npm && npm publish
 import { build, emptyDir } from "https://deno.land/x/dnt@0.23.0/mod.ts";
 
 await emptyDir("./npm");
